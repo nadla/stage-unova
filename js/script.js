9dd502a58c7f9e167b31741cce9bae82e160a8b9
nadla <bakyassir@free.fr>
2021-11-19 22:04:02 +0100

    Stage Unova

---
+++ js/script.js
@@ -0,0 +1,5 @@
+if (confirm('Voulez-vous ouvrir cette image dans un nouvel onglet ?')) {
+}
+
+var image = document.getElementById("myImage").src="rose-rose.jpg";
+alert("OnLoad image"+image );
---
+++ php/traitement.php
@@ -0,0 +1,3 @@
+<?php
+
+echo 'merci pour votre message :)';
